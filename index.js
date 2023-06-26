8dfea6fc8c73c4cf6062c8e9867061c9016dec4d
v4leng <valem.g_13@hotmail.com>
2023-06-26 16:37:22 -0300

    servidor levantado

---
+++ index.js
@@ -0,0 +1,23 @@
+// Configuración del server
+const db = require("./db/index");
+const express = require("express");
+const app = express();
+/* const router = require("./routes"); */
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+/* app.use("/api", router);
+ */
+db.sync({ force: false }).then(() => {
+  console.log("db conectada");
+  app.listen(3001, () => console.log("Servidor escuchando en el puerto 3001"));
+});
