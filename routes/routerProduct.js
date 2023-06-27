cdec211a04c3f3f3b8f2f7d2e7b45eb98d8bca04
v4leng <valem.g_13@hotmail.com>
2023-06-27 17:59:35 -0300

    router-product-detail-2

--- routes/routerProduct.js
+++ routes/routerProduct.js
@@ -3,6 +3,18 @@ const routerProduct = express.Router();
 
 const Product = require("../models/Product");
 
+// Ruta para obtener un producto
+routerProduct.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Product.findByPk(id)
+    .then((product) => {
+      if (!product) {
+        return res.status(404).json({ error: "El producto  no existe" });
+      }
+      res.send(product);
+    })
+    .catch((error) => res.status(500).json({ error: "Error en el servidor" }));
+});
 
 // Ruta para obtener todos los productos
 routerProduct.get("/products", async (req, res) => {
