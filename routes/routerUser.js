239593624e4e94a8fb21539b2522e1d12aac24f9
v4leng <valem.g_13@hotmail.com>
2023-06-29 22:34:47 -0300

    bug

--- routes/routerUser.js
+++ routes/routerUser.js
@@ -3,8 +3,7 @@ const routerUser = express.Router();
 const { generateToken } = require("../config/tokens");
 const bcrypt = require("bcrypt");
 const User = require("../models/Users");
-const {validateAuth} = require("../middleware/index.js")
-
+const { validateAuth } = require("../middleware/index.js");
 
 routerUser.post("/register", (req, res) => {
   const { name, lastname, email, password } = req.body;
@@ -44,11 +43,10 @@ routerUser.post("/login", async (req, res) => {
   }
 });
 
-
 routerUser.get("/me", validateAuth, (req, res) => res.send(req.user));
-routerUser.post("/logout", (req, res, next) => {
-  res.clearCookie("token").sendStatus(204);
+routerUser.post("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.sendStatus(204);
 });
-
 
 module.exports = routerUser;
